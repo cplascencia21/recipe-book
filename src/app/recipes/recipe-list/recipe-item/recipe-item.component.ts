7ccd846aaa3eb13c331fb36467723503356dde52
unknown <cplascencia21@yahoo.com>
2018-06-04 16:12:15 -0700

    recipe-book

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [{
+    name: 'A new recipe',
+    description: 'test recipe',
+    imagePath: 'https://www.chowstatic.com/assets/recipe_photos/30260_RecipeImage_fullsize_butternut_panzanella.jpg'
+  },
+  new Recipe('A Test Recipe',
+  'This is a sample test',
+  'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/Seasoned-Tilapia-Fillets_EXPS_SDFM18_41183_C10_10_6b.jpg')
+];
 
   constructor() { }
 
